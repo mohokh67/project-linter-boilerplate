a799e76e222b8d4570fbe4f539affedb64302169
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-11-16 16:55:23 +0000

    testing to see if it works

--- willNotBeLintByGitCommit.js
+++ willNotBeLintByGitCommit.js
@@ -1,6 +1,6 @@
 let x;
 
-function xsss(argument1, argument1b, argument1f) {
-  console.log(            "I will not be fixed");
+function dummmmmy(argument1, argument1b, argument1f) {
+  console.log(    "I will not be fixed");
   const b = "nooooo";
 }
